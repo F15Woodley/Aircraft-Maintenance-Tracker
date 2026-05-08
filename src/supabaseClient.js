32d65359db5f496052bf08cb20a1c920eb1cd10c
F15Woodley <rwoodley@digitalaerial.com>
2026-05-08 16:20:54 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oaiffuvwxdukjfaudcpe.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9haWZmdXZ3eGR1a2pmYXVkY3BlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTgwMTMsImV4cCI6MjA5MzgzNDAxM30.nJXAo46aRRdvCzy4cujb-m9nOptv2Y5PhqxgRjlsas8";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
